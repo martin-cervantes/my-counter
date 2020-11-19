f7ae5e25597c1ca729a8d403f25d0472c258c16a
Martin Cervantes <cervantes.martine@gmail.com>
2020-11-19 14:09:35 -0700

    Import the Provider and wrap the contents of App with it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from './containers/App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  <Provider/>, document.getElementById('root'));
