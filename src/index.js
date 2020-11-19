4aae849af9f9ace70078ebea68114aa8655f8d08
Martin Cervantes <cervantes.martine@gmail.com>
2020-11-19 14:11:14 -0700

    Import and create the Store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './containers/App';
 
+const store = createStore();
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   <Provider/>, document.getElementById('root'));
