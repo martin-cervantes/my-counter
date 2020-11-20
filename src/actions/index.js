a2cac86d5db56c0766e9899fa0c37c4d44f77720
Martin Cervantes <cervantes.martine@gmail.com>
2020-11-19 17:15:23 -0700

    Add file for reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,9 @@ export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
 export function counterIncrement() {
-  return ({ type: INCREMENT });
+  return { type: INCREMENT };
 }
 
 export function counterDecrement() {
-  return ({ type: DECREMENT });
+  return { type: DECREMENT };
 }
--- src/index.js
+++ src/index.js
@@ -2,32 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import counterReducer from './reducers';
 
 import Counter from './containers/Counter';
 
-
-import { INCREMENT, DECREMENT } from './actions';
-
-const initialState = {
-  count: 0
-};
-
-function reducer(state = initialState, action) {
-  switch(action.type) {
-    case INCREMENT:
-      return {
-        count: state.count + 1
-      };
-    case DECREMENT:
-      return {
-        count: state.count - 1
-      };
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
+const store = createStore(counterReducer);
 
 const App = () => (
   <Provider store={store}>
